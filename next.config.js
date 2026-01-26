c388e526fed6cdbb1b7a2a84447f2b58a0340bc2
Mariusz Matusiewicz <mariuszmmm@op.pl>
2026-01-26 10:28:21 +0100

    Refactor next.config.js: standardize formatting and improve readability

--- next.config.js
+++ next.config.js
@@ -1,19 +1,21 @@
+const basePath = process.env.NEXT_PUBLIC_BASE_URL || "";
+
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
+  output: "export",
   images: { unoptimized: true },
   compiler: { styledComponents: true },
   webpack(config) {
-    config.module.rules.push(
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: ['@svgr/webpack'],
-      }
-    );
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
     return config;
   },
   trailingSlash: true,
-}
+  basePath,
+  assetPrefix: basePath,
+};
 
 module.exports = nextConfig;
