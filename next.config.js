30d961b300692d94287d68e5247bc928119f313c
Mariusz Matusiewicz <mariuszmmm@op.pl>
2026-01-26 10:54:52 +0100

    test

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
@@ -12,6 +14,8 @@ const nextConfig = {
     return config;
   },
   trailingSlash: true,
+  basePath: isGithubPages ? "/Serwis-RTVAGD" : "",
+  assetPrefix: isGithubPages ? "/Serwis-RTVAGD/" : "",
 };
 
 module.exports = nextConfig;
